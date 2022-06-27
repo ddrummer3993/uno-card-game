a14f9397a41658ebd8bd27e23f7b9ae4fbe11354
Neten Shrestha <netenshrestha@gmail.com>
2022-06-27 13:43:05 -0400

    Created Enums for Symbol and Color

---
+++ src/main/java/com/sde_uno/unocardgame/model/Color.java
@@ -0,0 +1,18 @@
+package com.sde_uno.unocardgame.model;
+
+public enum Color {
+
+    RED("R"),
+    GREEN("G"),
+    BLUE("B"),
+    YELLOW("Y");
+
+    private final String colorSymbol;
+
+    Color(String colorSymbol){
+        this.colorSymbol= colorSymbol;
+    }
+    public String getColorSymbol(){
+        return colorSymbol;
+    }
+}
