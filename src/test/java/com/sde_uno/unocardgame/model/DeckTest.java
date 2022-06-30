ffb0090f8496d21a755f0947f0c6188bc8dc94f8
Nina W <103607735+woownina@users.noreply.github.com>
2022-06-29 20:12:55 -0500

    Worked on test class... merge in progress

--- src/test/java/com/sde_uno/unocardgame/model/DeckTest.java
+++ src/test/java/com/sde_uno/unocardgame/model/DeckTest.java
@@ -2,6 +2,7 @@ package com.sde_uno.unocardgame.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.NoSuchElementException;
@@ -9,6 +10,14 @@ import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
+
+    @Test
+    public void testHashCode() {
+        Deck deck1 = new Deck();
+        Deck deck2 = new Deck();
+        // equal objects must have equal hashcode
+        assertNotEquals(deck1.hashCode(), deck2.hashCode());
+    }
 
     @Test
     void resetDeck() {
@@ -25,20 +34,26 @@ class DeckTest {
     }
 
     @Test
-    void drawCard() {
+    void draw_cardsRemaining() {
        Deck deck = new Deck();
-
-
     }
 
 
     @Test
-    void getRemaining() {
+    void draw_noCardsRemaining() throws NoSuchElementException {
         Deck deck = new Deck();
+        try {
+            deck.draw();
+            fail("Should throw NoSuchElementException");
+        }
+        catch (NoSuchElementException e) {
+            assertEquals("Deck is empty!", );
+        }
+
     }
 
     @Test
-    void getDeckSize() {
+    public void getDeckSize() {
         Deck deck = new Deck();
         assertEquals(100,deck.getDeckSize());
     }
